787986865a1a54994d11efc0b352331caa714843
dnsdigitaltech <dnssites@gmail.com>
2020-09-16 22:56:05 -0300

    Iniciando Projeto - Endpoint salvando Produto

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -23,22 +24,31 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/", name="index", methods={"POST"})
+     * @Route("/", name="create", methods={"POST"})
      */
-    public function creaate()
+    public function create(Request $request)
     {
+        $productData = $request->request->all();
+
         $product = new Product();
-        $product->setName("Primeiro Produto");
-        $product->setDescription("Descrição Produto");
-        $product->setContent("COnteúdo Produto");
-        $product->setSlug("primeiro-produto");
-        $product->setPrice(1999);
+        $product->setName($productData['name']);
+        $product->setDescription($productData['description']);
+        $product->setContent($productData['content']);
+        $product->setSlug($productData['slug']);
+        $product->setPrice($productData['price']);
         $product->setIsActive(true);
         $product->setCreatedAt(new \DateTime("now", new \DateTimeZone('America/Sao_Paulo')));
         $product->setUpdatedAt(new \DateTime("now", new \DateTimeZone('America/Sao_Paulo')));
+
+        $doctrine = $this->getDoctrine()->getManager();
+
+        $doctrine->persist($product);
+        $doctrine->flush();
 
         return $this->json([
             'message' => 'Produto criado com sucesso!'
         ]);
     }
+
+    
 }
